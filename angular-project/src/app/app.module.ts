7d78e44d607764c1259af5a6639de89477ea74f5
NishitaSodhia <sodhiablue@gmail.com>
2021-04-10 22:04:10 +0530

    Created project and components

---
+++ angular-project/src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { ReportCardComponent } from './report-card/report-card.component';
+import { ExamComponent } from './exam/exam.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { AddQuestionComponent } from './add-question/add-question.component';
+import { RemoveQuestionComponent } from './remove-question/remove-question.component';
+import { ResultComponent } from './result/result.component';
+import { SearchUserComponent } from './search-user/search-user.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AdminComponent,
+    RegisterComponent,
+    LoginComponent,
+    HomeComponent,
+    AboutUsComponent,
+    ResetPasswordComponent,
+    ReportCardComponent,
+    ExamComponent,
+    AdminDashboardComponent,
+    UserDashboardComponent,
+    AddQuestionComponent,
+    RemoveQuestionComponent,
+    ResultComponent,
+    SearchUserComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ angular-project/src/app/remove-question/remove-question.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-remove-question',
+  templateUrl: './remove-question.component.html',
+  styleUrls: ['./remove-question.component.css']
+})
+export class RemoveQuestionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
