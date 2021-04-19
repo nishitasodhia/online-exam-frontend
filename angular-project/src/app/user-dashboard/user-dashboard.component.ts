82773648e0e3ced2f35067f39240cf4f30107adb
aakash <aakashtiwari15m@gmail.com>
2021-04-19 21:22:18 +0700

    Modified user-dashboard

--- angular-project/src/app/user-dashboard/user-dashboard.component.ts
+++ angular-project/src/app/user-dashboard/user-dashboard.component.ts
@@ -7,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDashboardComponent implements OnInit {
   username:string;
+  data: any;
   constructor() { }
 
   ngOnInit(): void {
     this.username=sessionStorage.getItem('userName');
+    console.log(this.data)
   }
  
 
